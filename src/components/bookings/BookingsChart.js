a6ca84b722c430c04f0f50e67c9699b442d24d25
Mark Boatman <markjboat@yahoo.com>
2021-08-15 21:27:59 +0800

    Video 23 end series, finished chart for booking price ranges

--- src/components/bookings/BookingsChart.js
+++ src/components/bookings/BookingsChart.js
@@ -1,26 +1,60 @@
 import React from 'react';
+// alias Bar to BarChart
+import { Bar as BarChart } from 'react-chartjs';
 
-// some kind of enumeration this ""
 const BOOKINGS_BUCKETS = {
-  Cheap: 100,
-  Normal: 200,
-  Expensive: 10000000,
+  Cheap: {
+    min: 0,
+    max: 100,
+  },
+  Normal: {
+    min: 100,
+    max: 200,
+  },
+  Expensive: {
+    min: 200,
+    max: 10000000,
+  },
 };
-// @22:00
+// @29:30
 const BookingsChart = (props) => {
-  const output = {};
+  // this executes when component loads
+  const chartData = { labels: [], datasets: [] };
+  let bookingCosts = [];
   for (const bucket in BOOKINGS_BUCKETS) {
     const filteredBookingsCount = props.bookings.reduce((prev, current) => {
-      if (current.price < BOOKINGS_BUCKETS[bucket]) {
+      if (
+        current.event.price < BOOKINGS_BUCKETS[bucket].max &&
+        current.event.price > BOOKINGS_BUCKETS[bucket].min
+      ) {
         return prev + 1;
       } else {
         return prev;
       }
     }, 0);
-    output[bucket] = filteredBookingsCount;
-  }
-  console.log(output);
-  return <p>The Chart</p>;
+    chartData.labels.push(bucket);
+    bookingCosts.push(filteredBookingsCount);
+    chartData.datasets.push({
+      data: bookingCosts,
+      fillColor: 'rgba(220,220,220,0.5)',
+      strokeColor: 'rgba(220,220,220,0.8)',
+      highlightFill: 'rgba(220,220,220,0.75)',
+      highlightStroke: 'rgba(220,220,220,1)',
+    });
+    // move reference to a copy of the current array
+    bookingCosts = [...bookingCosts];
+    // put a zero as the last value in the array
+    // so format will be correct for data: [bookingCosts
+    bookingCosts[bookingCosts.length - 1] = 0;
+  } // end for in
+
+  const chart = (
+    <div style={{ textAlign: 'center' }}>
+      <BarChart data={chartData} />
+    </div>
+  );
+  // return <Bar data={} />
+  return chart;
 };
 
 export default BookingsChart;
